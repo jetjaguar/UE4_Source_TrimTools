aa21ea48e0a37b2cea08b4d61f0f761857a62e2f
Patrick Finegan <pfinegan@pfinegan.name>
2019-09-22 16:08:03 -0400

    Restructure so that it can be merged into UE4 more easily, Remove TrimSize.bat implementation in favor of .gitdepsignore platform-agnostic implementation

--- Engine/Build/EZBuild/EZPZ.bat
+++ Engine/Build/EZBuild/EZPZ.bat
@@ -1,20 +1,23 @@
 @echo off
 TITLE Setting up UE4 Project
 ::Assuming Default Location of Engine\Build\EZPZ
-SET "TrimSetupConf=..\TrimSize\TrimSetup.bat"
+SET "gitdeps=..\TrimSize\.gitdepsignore"
+SET "gitdepsfinal=..\..\..\.gitdepsignore"
 SET "BinaryMarker=..\BinaryPrerequisitesMarker.dat"
 SET "SetupLocation=..\..\..\Setup.bat"
 SET "GPFLocation=..\..\..\GenerateProjectFiles.bat"
 
 ::Read TrimSize system for platforms to _not_ download
 SET platformExcludes=
-IF EXIST "%TrimSetupConf%" (
-	call "%TrimSetupConf%"
+IF EXIST "%gitdeps%" (
+	IF NOT EXIST "%gitdepsfinal%" (
+		COPY %gitdeps% %gitdepsfinal%
+	)	
 )
 
 IF EXIST "%BinaryMarker%" (
 	ECHO "Setup already done"
 ) ELSE (
-	call %SetupLocation% %platformExcludes%
+	call %SetupLocation% %platformExcludes% -dry-run
 )
 call %GPFLocation%
--- TrimSize/TrimSetup.bat
+++
@@ -1,5 +0,0 @@
-set exMac=-exclude=Mac -exclude=osx32 -exclude=osx64 -exclude=osx
-set exMob=-exclude=Android -exclude=android -exclude=HTML5 -exclude=html5 -exclude=IOS -exclude=ios -exclude=TVOS -exclude=tvos
-set exLin=-exclude=Linux -exclude=linux -exclude=Linux32 -exclude=linux32 -exclude=Linux64 -exclude=linux64
-
-set platformExcludes= -exclude=Win32 %exMac% %exMob%
